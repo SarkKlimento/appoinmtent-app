28667eec3e16435d66a0935cf96d83a121f3b7b4
root <shadowdragonwizard@gmail.com>
2019-10-23 21:26:57 +0300

    Changed auth

--- src/app/shared/services/salesforce-restcallout-service.service.ts
+++ src/app/shared/services/salesforce-restcallout-service.service.ts
@@ -14,7 +14,7 @@ export class SalesforceRESTcalloutServiceService {
 
   constructor(private http: HttpClient, private route: ActivatedRoute) {
     this.consumerKey = "3MVG91BJr_0ZDQ4ts4wXWZjdsb6SUrhvlOJodd2MCjLiglKDaqpQrnEfOgMb8iluoTu8h8FknH7DB1ME1Hp7g";
-    this.baseEndpoint = 'https://sark-klimento-dev-ed.lightning.force.com/services/apexrest/';
+    this.baseEndpoint = 'https://sark-klimento-dev-ed.my.salesforce.com/services/apexrest/';
     this.redirect_uri = 'https://sark-appointment-app.herokuapp.com/';
   }
 
