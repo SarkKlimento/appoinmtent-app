b60137d3b8d2e3e220f9303c0524269835dc4c29
root <shadowdragonwizard@gmail.com>
2019-10-23 18:37:56 +0300

    Changed auth

--- server.js
+++ server.js
@@ -7,21 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/appointments-app'));
 
-app.get('/*', function (req, res) {
+app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname, '/dist/appointments-app/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/appointments-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-var oauth2 = require('salesforce-oauth2');
-app.get('/oauth2/auth', function (req, res) {
-  var uri = oauth2.getAuthorizationUrl({
-    redirect_uri: 'https://sark-appointment-app.herokuapp.com/',
-    client_id: '3MVG91BJr_0ZDQ4ts4wXWZjdsb6SUrhvlOJodd2MCjLiglKDaqpQrnEfOgMb8iluoTu8h8FknH7DB1ME1Hp7g',
-    scope: 'api'
-  });
-  console.log(res.redirect(uri));
-  return res.redirect(uri);
-})
