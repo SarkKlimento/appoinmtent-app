a4ad710a49c5c7ca2fe5f1b75a730d242b92ed56
mikhail_pantsevich <mikhail_pantsevich@senla.eu>
2019-10-25 10:43:50 +0300

    Reverted changes, added window test

--- src/app/shared/services/salesforce-restcallout-service.service.ts
+++ src/app/shared/services/salesforce-restcallout-service.service.ts
@@ -46,21 +46,15 @@ export class SalesforceRESTcalloutServiceService {
   getTokens(): Observable<Object> {
     return new Observable(observer => {
       const code = this.getCodeFromURL();
-      const tokenEndpoint = 'https://login.salesforce.com/services/oauth2/token';
-      const tokenBody = 'client_id='
+      const tokenEndpoint = 'https://login.salesforce.com/services/oauth2/token?client_id='
         + this.consumerKey +
         '&grant_type=authorization_code' +
         '&redirect_uri=' + this.redirect_uri +
         '&code=' + code;
-      const httpOptions = {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/x-www-form-urlencoded'
-        })
-      };
+
       console.log(tokenEndpoint);
-      console.log(tokenBody);
-      console.log(httpOptions);
-      this.http.post<Object>(tokenEndpoint, tokenBody, httpOptions).pipe().subscribe(next => {
+      window.open(tokenEndpoint, '_self');
+      this.http.post<Object>('tokenEndpoint', '').pipe().subscribe(next => {
 
         console.log(next);
         // TODO: send data to the cookie
