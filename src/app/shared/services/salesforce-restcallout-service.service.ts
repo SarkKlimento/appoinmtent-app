6e38ac600a05ac13b0d07072d9887f3a22321f54
mikhail_pantsevich <mikhail_pantsevich@senla.eu>
2019-10-25 13:15:41 +0300

    jquery fixes

--- src/app/shared/services/salesforce-restcallout-service.service.ts
+++ src/app/shared/services/salesforce-restcallout-service.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {ActivatedRoute} from '@angular/router';
-import * as $ from 'jquery';
+declare var $: any;
 
 @Injectable()
 export class SalesforceRESTcalloutServiceService {
